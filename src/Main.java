2878c13243acc5b9960d1e982b36b17cee422418
CyberJhin <drkapp@mail.ru>
2025-07-13 17:11:57 +0300

    Implemented file path parsing with existence check

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,39 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int correctFileCount = 0;
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+
+            String path = sc.nextLine().trim();
+
+            if (path.isEmpty()) {
+                System.out.println("Ошибка: путь не может быть пустым.");
+                continue;
+            }
+
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Ошибка: файл не существует.");
+                } else {
+                    System.out.println("Ошибка: указанный путь ведет к папке, а не к файлу.");
+                }
+                continue;
+            }
+
+            correctFileCount++;
+            System.out.println("Путь указан верно.");
+            System.out.println("Это файл номер " + correctFileCount);
+        }
     }
 }
